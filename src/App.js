169c9cc3fd19c7af3930dc1fbd88ce9996a368f5
saykot12 <saykot.cse43@gmail.com>
2022-03-30 13:43:54 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
+import Header from "./components/Header/Header";
+import Coins from "./components/Coins/Coins";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
     <div>
-      <h1>Crypto Cafe Starter Template</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
