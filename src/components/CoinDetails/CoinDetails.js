63d37a2b8e0d85b6313348af10534772c1817038
saykot12 <saykot.cse43@gmail.com>
2022-03-30 16:23:27 +0600

    spinar added

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 import Spinner from "../Spinner/Spinner";
 
 const CoinDetails = () => {
-  const { id } = useParams();
   const [coin, setCoin] = useState({});
   const [loading, setLoading] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
-    fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+    setLoading(true);
+    const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setCoin(data);
@@ -22,10 +24,10 @@ const CoinDetails = () => {
         <Spinner />
       ) : (
         <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
-          <div className="h-full grid grid-cols-1 md:grid-cols-2 justify-center justify-items-center gap-4 content-center">
+          <div className="h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 md:justify-items-around content-center">
             <div className="order-2 md:order-1">
               <h1 className="text-3xl">General Info:</h1>
-              <hr></hr>
+              <hr />
               <h1>Coin Name: {coin.name}</h1>
               <h1>Market Cap Rank: {coin.market_cap_rank}</h1>
               <h1>
