175c2e0d9bf26759cecee35cccd6eb4fa8834f25
saykot12 <saykot.cse43@gmail.com>
2022-03-30 16:09:22 +0600

    nasted route & not found page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import CoinDetails from "./components/CoinDetails/CoinDetails";
+import BdAddress from "./components/Contact/BdAddress";
+import UsAddress from "./components/Contact/UsAddress";
+import NotFound from "./components/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -16,8 +19,12 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/coins" element={<Coins />} />
         <Route path="/coin-details/:id" element={<CoinDetails />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />}>
+          <Route path="bd-address" element={<BdAddress />} />
+          <Route path="us-address" element={<UsAddress />} />
+        </Route>
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
